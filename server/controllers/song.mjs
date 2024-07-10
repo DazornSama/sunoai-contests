4c5239124cf9e4aca44fd872600ed9e2a6fad8dd
DazornSama <marcodz554@gmail.com>
2024-07-10 14:14:14 +0200

    Fixed unicode parsing in scraper

--- server/controllers/song.mjs
+++ server/controllers/song.mjs
@@ -3,6 +3,33 @@ import Song, { SongFromMongo } from "../models/song.mjs";
 
 const COLLECTION_NAME = "songs";
 
+const utf8Decode = (utf8String) => {
+  if (typeof utf8String != "string")
+    throw new TypeError("parameter ‘utf8String’ is not a string");
+  // note: decode 3-byte chars first as decoded 2-byte strings could appear to be 3-byte char!
+  const unicodeString = utf8String
+    .replace(
+      /[\\u00e0-\\u00ef][\\u0080-\\u00bf][\\u0080-\\u00bf]/g, // 3-byte chars
+      function (c) {
+        // (note parentheses for precedence)
+        var cc =
+          ((c.charCodeAt(0) & 0x0f) << 12) |
+          ((c.charCodeAt(1) & 0x3f) << 6) |
+          (c.charCodeAt(2) & 0x3f);
+        return String.fromCharCode(cc);
+      }
+    )
+    .replace(
+      /[\\u00c0-\\u00df][\\u0080-\\u00bf]/g, // 2-byte chars
+      function (c) {
+        // (note parentheses for precedence)
+        var cc = ((c.charCodeAt(0) & 0x1f) << 6) | (c.charCodeAt(1) & 0x3f);
+        return String.fromCharCode(cc);
+      }
+    );
+  return unicodeString;
+};
+
 export const getOne = async (request, response) => {
   try {
     const songId = request.params.id.trim();
@@ -191,8 +218,9 @@ export const getInfo = async (request, response) => {
     let regex = /\\"title\\":\\"(.[^\\]*)\\"/g;
     const title = regex.exec(body)[1];
 
-    regex = /\\"metadata\\":{\\"tags\\":\\"(.[^\\]*)\\",/g;
-    const prompt = regex.exec(body)[1];
+    regex = /(?<=\\"tags\\":\\")[^\"]*/g;
+    let prompt = utf8Decode(regex.exec(body)[0]);
+    prompt = prompt.substring(0, prompt.length - 1);
 
     regex = /\\"display_name\\":\\"(.[^\\]*)\\",/g;
     const author = regex.exec(body)[1];
